7d17639ee8bb07947e005e962f29b305dfdb0f73
Thai Nguyen <discoverylight@yahoo.com>
2020-01-22 15:35:16 -0800

    completed eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,18 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n <= 1:
+    return 1
+  else:
+    ways_to_eat = 0
+  for i in range(1, min(n+1,4)):
+    ways_to_eat += eating_cookies(n - i)
+  
+  return ways_to_eat
+  
+  
+
+print(eating_cookies(3))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
